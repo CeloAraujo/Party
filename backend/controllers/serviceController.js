4be4f5dca130bd020a3047d4b35168e973020cb6
celoaraujo <celoparaujo99@gmail.com>
2024-05-30 13:04:44 -0300

    inserção dados no BD

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -1,5 +1,21 @@
 const { Service: ServiceModel } = require("../models/Service");
 
-const serviceController = {};
+const serviceController = {
+  create: async (req, res) => {
+    try {
+      const service = {
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image: req.body.image,
+      };
+
+      const response = await ServiceModel.create(service);
+      res.status(201).json({ response, msg: "Serviço criado!" });
+    } catch (error) {
+      console.log(`Erro:${error}`);
+    }
+  },
+};
 
 module.exports = serviceController;
--- backend/routes/services.js
+++ backend/routes/services.js
@@ -2,4 +2,8 @@ const router = require("express").Router();
 
 const serviceController = require("../controllers/serviceController");
 
+router
+  .route("/services")
+  .post((req, res) => serviceController.create(req, res));
+
 module.exports = router;
