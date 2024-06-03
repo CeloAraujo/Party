08b6b5b77f53d77bbbc6990cc6825783c0b5b6e5
celoaraujo <celoparaujo99@gmail.com>
2024-06-03 16:06:03 -0300

    update serviços

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -32,9 +32,53 @@ const serviceController = {
 
       if (!service) {
         res.status(404).json({ msg: "Serviço não encontrado" });
+        return;
       }
 
       res.json(service);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const service = await ServiceModel.findById(id);
+
+      if (!service) {
+        res.status(404).json({ msg: "Serviço não encontrado" });
+        return;
+      }
+
+      const deletedService = await ServiceModel.findByIdAndDelete(id);
+
+      res
+        .status(200)
+        .json({ deletedService, msg: "Serviço excluído com sucesso" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const service = {
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image: req.body.image,
+      };
+
+      const updatedService = await ServiceModel.findByIdAndUpdate(id,service);
+
+      if (!updatedService) {
+        res.status(404).json({ msg: "Serviço não encontrado" });
+        return;
+      }
+
+      res
+        .status(200)
+        .json({ service, msg: "Serviço atualizado com sucesso" });
     } catch (error) {
       console.log(error);
     }
