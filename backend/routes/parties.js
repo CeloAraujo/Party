260513a66fd2190188d178dcbb41076084a0565e
celoaraujo <celoparaujo99@gmail.com>
2024-06-03 17:04:02 -0300

    criando festa

--- backend/routes/parties.js
+++ backend/routes/parties.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
 const partyController = require("../controllers/partyController");
-
+router.route("/parties").post((req, res) => partyController.create(req, res));
 
 module.exports = router;
