1307d3d15b74d0283980ed25672febe3dd78f79c
celoaraujo <celoparaujo99@gmail.com>
2024-06-04 15:35:22 -0300

    get festa individual,delete and update

--- backend/controllers/partyController.js
+++ backend/controllers/partyController.js
@@ -42,6 +42,65 @@ const partyController = {
       console.log(error);
     }
   },
+  get: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const party = await PartyModel.findById(id);
+
+      if (!party) {
+        res.status(404).json({ msg: "Festa não encontrada" });
+        return;
+      }
+
+      res.json(party);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const party = await PartyModel.findById(id);
+
+      if (!party) {
+        res.status(404).json({ msg: "Festa não encontrada" });
+        return;
+      }
+      const deletedParty = await PartyModel.findByIdAndDelete(id);
+
+      res.status(200).json({ deletedParty, msg: "Festa excluída com sucesso" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const party = {
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        budget: req.body.budget,
+        image: req.body.image,
+        services: req.body.services,
+      };
+      if (party.services && !checkPartyBudget(party.budget, party.services)) {
+        res.status(406).json({ msg: "O seu orçamento não é suficiente!" });
+        return;
+      }
+
+      const updatedParty = await PartyModel.findByIdAndUpdate(id, party);
+
+      if (!updatedParty) {
+        res.status(404).json({ msg: "Serviço não encontrado" });
+        return;
+      }
+
+      res.status(200).json({ party, msg: "Serviço atualizado com sucesso" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = partyController;
--- backend/routes/parties.js
+++ backend/routes/parties.js
@@ -5,4 +5,13 @@ router.route("/parties").post((req, res) => partyController.create(req, res));
 
 router.route("/parties").get((req, res) => partyController.getAll(req, res));
 
+router.route("/parties/:id").get((req, res) => partyController.get(req, res));
+
+router
+  .route("/parties/:id")
+  .delete((req, res) => partyController.delete(req, res));
+router
+  .route("/parties/:id")
+  .put((req, res) => partyController.update(req, res));
+
 module.exports = router;
