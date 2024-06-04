65054de274d91d5eb23339e533ee6140c472d268
celoaraujo <celoparaujo99@gmail.com>
2024-06-04 17:30:40 -0300

    componente navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import { Outlet } from 'react-router-dom'
-import './App.css'
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import "./App.css";
+
+//components
+import Navbar from "./components/Navbar";
 
 function App() {
-
-
   return (
     <>
-     <div>
-      <h1>Fun Time</h1>
-      <Outlet/>
-     </div>
+      <div>
+        <Navbar />
+        <Outlet />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
