0b905c2f39055475c121682c316970a7ac6352ab
celoaraujo <celoparaujo99@gmail.com>
2024-06-07 17:00:25 -0300

    changed backend url

--- frontend/src/axios/config.js
+++ frontend/src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const partyFetch = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://fun-time-y72m.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
